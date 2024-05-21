de159968da095654389784480e1f320d6fc63c1c
Nejma Nefertiti <159094527+StarTemple@users.noreply.github.com>
2024-05-20 20:20:39 -0400

    Update test_grade_calculator.py

--- test_grade_calculator.py
+++ test_grade_calculator.py
@@ -3,4 +3,4 @@ from grade_calculator import calculate_average_grade
 
 def test_calculate_average_grade():
     with patch('builtins.input', side_effect=['John Doe', '85', '90', '92']):
-        assert calculate_average_grade() == ('John Doe', 89)
+        assert calculate_average_grade() == ('John Doe', 89) 
